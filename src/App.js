cf2bb00344e3dc675dfcdf24139369d96e176e88
sunwoonam <swoonam1004@gmail.com>
2024-07-02 00:08:10 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [amount, setAmount] = useState("");
+  const [btcPrice, setBtcPrice] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        const bitcoin = json.find((coin) => coin.symbol === "BTC");
+        setBtcPrice(bitcoin.quotes.USD.price);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleAmountChange = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const calculateBTC = () => {
+    if (btcPrice && amount) {
+      return amount / btcPrice;
+    }
+    return 0;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coin Calculator {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <>
+          <select>
+            {coins.map((coin) => (
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          <div>
+            <input
+              type="number"
+              placeholder="Amount in USD"
+              value={amount}
+              onChange={handleAmountChange}
+            />
+            <p>
+              You can get {calculateBTC()} BTC for ${amount}
+            </p>
+          </div>
+        </>
+      )}
     </div>
   );
 }
